4c5ace373083e12b90c306868bad0a97407e4d91
Jake Nations <jnations@8thlight.com>
2014-10-28 14:50:14 -0500

    Update tests to convert Binary to String

--- spec/http_server/router_spec.clj
+++ spec/http_server/router_spec.clj
@@ -17,39 +17,42 @@
   (after (write-to-test ""))
 
   (it "returns file contnets from a GET /file request"
-    (should= "HTTP/1.1 200 OK\r\nContent-Length:15\r\nContent-Type:text/plain\r\n\r\nfile1 contents"
-             (router path {:action "GET" :location "/file1"})))
+    (should= "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:15\r\n\r\nfile1 contents\n"
+             (String. (router path {:action "GET" :location "/file1"}))))
 
   (it "returns directory links from GET /"
-    (should= "HTTP/1.1 200 OK\r\nContent-Length:115\r\n\r\n<!DOCTYPE html><html><head><title>directory</title></head><body><a href=\"/file.txt\">file.txt</a><br></body></html>" 
-             (router test-path {:action "GET" :location "/"})))
+    (should= "HTTP/1.1 200 OK\r\nContent-Length:114\r\n\r\n<!DOCTYPE html><html><head><title>directory</title></head><body><a href=\"/file.txt\">file.txt</a><br></body></html>" 
+             (String. (router test-path {:action "GET" :location "/"}))))
   
   (it "returns 404 when trying to read nonexistent file"
     (should= "HTTP/1.1 404 NOT FOUND\r\n\r\n" 
-             (router path {:action "GET" :location "/foobar"})))
+             (String. (router path {:action "GET" :location "/foobar"}))))
 
   (it "returns allow header with GET POST OPTIONS PUT HEAD from options"
     (should=
       "HTTP/1.1 200 OK\r\nAllow:GET,HEAD,POST,OPTIONS,PUT\r\n\r\n"
-      (router path  {:action "OPTIONS" :location "/"})))
+      (String. (router path  {:action "OPTIONS" :location "/"}))))
 
   (it "appends body of the request to the requested file POST"
     (write-to-test "test")
     (should= ok 
-             (router path {:action "POST" :location "/test"} "test"))
+             (String. (router path {:action "POST" :location "/test"} "test")))
     (should= "testtest" (slurp (str path "/test"))))
   
   (it "PUT overwrites current file content"
     (write-to-test "FAIL")
     (should= ok 
-             (router path {:action "PUT" :location "/test"} "PUT test"))
+             (String. (router path {:action "PUT" :location "/test"} "PUT test")))
     (should= "PUT test" (slurp (str path "/test"))))
   (it "should not put to protected file"
     (should= "HTTP/1.1 405 METHOD NOT ALLOWED\r\n\r\n"
-             (router path {:action "PUT" :location "/file1"} "file1 contents")))
+             (String.
+               (router path
+                       {:action "PUT" :location "/file1"} "file1 contents"))))
 
   (it "deletes file contents with DELETE"
     (write-to-test "FAIL")
     (should= ok 
-             (router path {:action "DELETE" :location "/test"}))
+             (String. 
+               (router path {:action "DELETE" :location "/test"})))
     (should= "" (slurp (str path "/test")))))
