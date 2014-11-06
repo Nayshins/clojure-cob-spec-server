1090cf604ae1d5505eb1237e39ec70de6bd42a0f
Jake Nations <jnations@8thlight.com>
2014-11-06 11:54:47 -0600

    Remove Commons IO dependency

--- project.clj
+++ project.clj
@@ -12,8 +12,7 @@
                                                      javax.jms/jms
                                                      com.sun.jmdk/jmxtools
                                                      com.sun.jmx/jmxri]]
-                 [base64-clj "0.1.1"]
-                 [commons-io/commons-io "2.4"]]
+                 [base64-clj "0.1.1"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :plugins [[speclj "3.1.0"]]
   :test-paths ["spec"]
