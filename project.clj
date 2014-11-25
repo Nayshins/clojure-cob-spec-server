44f55bfc96e938c7ca507188ab9521d03d02f2e5
Jake Nations <jnations@8thlight.com>
2014-11-25 14:16:05 -0600

    Updated to used clojure http server 0.2.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [http-server "0.1.0-SNAPSHOT"]]
+                 [http-server "0.2.0"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :plugins [[speclj "3.1.0"]]
   :test-paths ["spec"]
--- src/http_server/startup.clj
+++ src/http_server/startup.clj
@@ -1,25 +1,50 @@
 (ns http-server.startup
-  (:require [clojure.tools.cli :refer [parse-opts]]
-            [http-server.server :refer :all]
-            [http-server.config :refer :all])
-  (:import [java.io File])
-  (:gen-class))
+  (:require [http-server.cli-options :refer [parse]]
+            [http-server.server :as server]
+            [http_server.handlers :as handlers-helper]))
 
-(set! *warn-on-reflection* true)
+(def directory "./public")
 
-(def directory (atom ""))
+(defn authenticate [request]
+  (let [headers (request :headers)] 
+    (if (headers :Authorization)
+      (http-server.router/get-route request directory)
+      {:status 401 :body (byte-array (.getBytes "Authentication required"))})))
 
-(def cli-options
-  [["-p" "--port PORT" "Port Number"
-    :id :port
-    :default 5000
-    :parse-fn #(Integer/parseInt %)]
 
-   ["-d" "--directory DIRECTORY" "Directory of public folder"
-    :id :directory
-    :default (str (->  (java.io.File. "") .getAbsolutePath) "/public")]])
 
+(def directory-links (http-server.router/build-directory directory))
+
+(def routes [["GET" "/" directory-links]
+             ["PUT" "/file1" {:status 405}]
+             ["POST" "/text-file.txt" {:status 405}]
+             ["GET" "/redirect" {:status 301
+                                 :headers {"Location" "http://localhost:5000/"}}]
+             ["GET" "/logs" authenticate]])
+
+(defn app-router [request]
+  (some #(handlers-helper/check-route request %) routes))
+
+(defn parameters-router [request]
+  (prn request)
+  (let [query (clojure.string/split (request :location) #"\?")
+        location (first query)
+        params (second query)]
+    (prn params)
+    (if (nil? params)
+      nil
+      {:status 200 :body (http-server.router/decode-params params)})))
+
+(defn resource-router [request]
+  (http-server.router/router request directory))
+
+(defn not-found [request]
+  {:status 404})
+
+(def handlers [app-router parameters-router resource-router not-found])
 (defn -main [& args]
-  (let [{:keys [options arguments summary]} (parse-opts args cli-options)]
-    (reset! directory (options :directory))
-    (server (create-server-socket (options :port)) (options :directory))))
+  (let [cli-options (parse args)]
+    (prn "starting server")
+    (server/serve 
+      (server/create (cli-options :port)) 
+      handlers-helper/try-handlers handlers)))
