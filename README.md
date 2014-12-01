474e9074791425ac5196d58ad91cb9e9703b1929
Jake Nations <jnations@8thlight.com>
2014-12-01 09:34:34 -0600

    Update server to use clojure http server 0.2.2

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Simple Http Server Written in Clojure
 
-## Requirments 
+## Requirements 
 Leiningen 2.5 is required in order to run the server. Installation instructions can be found [here](http://leiningen.org/)
 
 Cob Spec test suite is required to run the acceptance tests. The test suite and instructions can be found [here](https://github.com/8thlight/cob_spec)
 
 ## Initial Setup
-1. Clone repository into directory of your chosing
+1. Clone repository into directory of your choosing
 2. cd into the clojure-http-server directory
 3. run $lein deps to install dependencies
 
@@ -15,5 +15,9 @@ This server is configured to meet the specifications of the Cob Spec test suite,
 ```
 $ lein run
 ```
-
-Once the server is running you can run the Cob Spec suite against it.
+## Cob Spec Setup
+Cob spec will require a minimal amount of setup, and for this server it requires
+that you set the public directory to the public directory in the
+clojure-cob-spec server directory. Once cob spec is set up, you can run the
+suite against the running server by clicking on the suite button that is on the fitnesse
+page.
--- src/http_server/startup.clj
+++ src/http_server/startup.clj
@@ -1,6 +1,7 @@
 (ns http-server.startup
-  (:require [http-server.cli-options :refer [parse]]
+  (:require [http-server.cli-options :as cli-options]
             [http-server.server :as server]
+            [http-server.resource-handler :as resource-handler]
             [http_server.handlers :as handlers-helper]))
 
 (def directory "./public")
@@ -8,13 +9,15 @@
 (defn authenticate [request]
   (let [headers (request :headers)] 
     (if (headers :Authorization)
-      (http-server.router/get-route request directory)
+      (resource-handler/get-route request directory)
       {:status 401 :body (byte-array (.getBytes "Authentication required"))})))
 
-(def directory-links (http-server.router/build-directory directory))
+(defn directory-links [directory]
+  (resource-handler/build-directory directory))
 
-(def routes [["GET" "/" directory-links]
+(def routes [["GET" "/" (directory-links directory)]
              ["PUT" "/file1" {:status 405}]
+             ["PUT" "/these" {:status 200}]
              ["POST" "/text-file.txt" {:status 405}]
              ["GET" "/redirect" {:status 301
                                  :headers {"Location" "http://localhost:5000/"}}]
@@ -24,23 +27,22 @@
   (some #(handlers-helper/check-route request %) routes))
 
 (defn parameters-router [request]
-  (prn request)
   (let [query (clojure.string/split (request :location) #"\?")
         location (first query)
         params (second query)]
     (if (nil? params)
       nil
-      {:status 200 :body (http-server.router/decode-params params)})))
+      {:status 200 :body (resource-handler/decode-params params)})))
 
 (defn resource-router [request]
-  (http-server.router/router request directory))
+  (resource-handler/router request directory))
 
 (defn not-found [request]
   {:status 404})
 
 (def handlers [app-router parameters-router resource-router not-found])
 (defn -main [& args]
-  (let [cli-options (parse args)]
+  (let [cli-options (cli-options/parse args)]
     (prn "starting server")
     (server/serve 
       (server/create (cli-options :port)) 
